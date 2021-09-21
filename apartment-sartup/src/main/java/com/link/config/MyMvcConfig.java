23108246f70301274a3c454fd08c13b1e2069213
陈艺明 <787837865@qq.com>
2021-09-21 15:53:06 +0800

    提交

---
+++ apartment-sartup/src/main/java/com/link/config/MyMvcConfig.java
@@ -0,0 +1,19 @@
+package com.link.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocalResolver();
+    }
+}
---
+++ apartment-sartup/src/main/java/com/link/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.link.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/user/login")
+    @ResponseBody
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Model model){
+        if (!StringUtils.isEmpty(username)&&"123456".equals(password)){
+            return "dashboard";
+        }else{
+            model.addAttribute("msg","用户名或密码错误");
+            return "index";
+        }
+
+    }
+}
